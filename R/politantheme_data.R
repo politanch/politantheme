3b88e4e47a7211c8546831af05f1455835694644
ThomasWilli <willi@uega.ch>
2015-01-14 11:13:13 +0100

    nexttry

--- R/politantheme_data.R
+++ R/politantheme_data.R
@@ -3,23 +3,36 @@
 ##' List with the data used by the palettes in the politan package.
 ##'
 ##' @export
-politantheme_data <- {
+#politantheme_data <- {
   ## x to hold value of list as I create it
-  x <- list()
-  x$politan <- list()
+  #x <- list()
+  #x$politan <- list()
   
   ## colors imported from here: http://www.javascripter.net/faq/rgbtohex.htm
   ## Colors picked
-  x$politan$bg <-
-    c(brown4 = rgb(139, 35, 35, max=255), # brown
-      coral3 = rgb(205, 91, 69, max=255), # kind of pinkbrown
-      coral = rgb(255, 127, 80, max=255),,
-      darkslategray3 = rgb(121, 205, 205, max=255), # gray bg for graphics blog
-      darkslategray4 = rgb(82, 139, 139, max=255),
-      darkslategray = rgb(47, 79, 79, max=255)      
-    ) 
+ # x$politan$bg <-
+   # c(brown4 = rgb(139, 35, 35, max=255), # brown
+      #coral3 = rgb(205, 91, 69, max=255), # kind of pinkbrown
+    #  coral = rgb(255, 127, 80, max=255),,
+    #  darkslategray3 = rgb(121, 205, 205, max=255), # gray bg for graphics blog
+    #  darkslategray4 = rgb(82, 139, 139, max=255),
+    #  darkslategray = rgb(47, 79, 79, max=255)      
+   # ) 
   ## Return
-  x
+  ##x
+  
+#}
+
+politan_col <- function(x){
+  colls <- c(
+  brown4 = rgb(139, 35, 35, max=255), # brown
+  coral3 = rgb(205, 91, 69, max=255), # kind of pinkbrown
+  coral = rgb(255, 127, 80, max=255),
+  darkslategray3 = rgb(121, 205, 205, max=255), # gray bg for graphics blog
+  darkslategray4 = rgb(82, 139, 139, max=255),
+  darkslategray = rgb(47, 79, 79, max=255)) 
+  
+  print(colls)
   
 }
 
