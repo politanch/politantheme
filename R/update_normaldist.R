e19f9ec642377bafe656662b806d93e40c2a8587
ThomasWilli <willi@uega.ch>
2015-09-05 12:56:49 +0200

    added missing ","

--- R/update_normaldist.R
+++ R/update_normaldist.R
@@ -6,16 +6,11 @@
 
 
 mean_updater <- function(variance_prior, mean_prior, variance_data, mean_data, n){
-
   meanpost <- ((1/variance_prior)*mean_prior + (n/variance_data)*mean_data) / ((1/variance_prior)+(n/variance_data))
-  
   return(meanpost)
-  
-  
 }
 
 var_updater <- function(variance_prior, variance_data, n){
-  
   varpred <- variance_data*((variance_prior/variance_data)+ 1/n)
   return(varpred)
 }
