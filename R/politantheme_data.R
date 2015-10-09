da60f50b6ec28624223047a271d7d7532ce54a1e
ThomasWilli <willi@uega.ch>
2015-10-09 23:57:03 +0200

    Party colors

--- R/politantheme_data.R
+++ R/politantheme_data.R
@@ -23,7 +23,12 @@
   
 #}
 
-politan_col <- function(x){
+politan_col <- function(){
+  cat("\nmaps_col() returns the colors for yes and no shares\n\nparty_col() returns a data.frame from the party colors, it takes the following arguments:\n\n     'party' = c('SVP', 'FDP', 'CVP', 'SP', 'EVP', 'GPS', 'BDP', 'glp')\n     'scheme' = c('light', 'normal')")
+
+  }
+
+maps_col <- function(x){
   colls <- c(
   brown4 = rgb(139, 35, 35, max=255), # brown
   coral3 = rgb(205, 91, 69, max=255), # kind of pinkbrown
@@ -34,5 +39,23 @@ politan_col <- function(x){
   
   print(colls)
   
+}
+
+
+party_col <- function(party=NULL, scheme=NULL){
+  
+  partycol <- data.frame(Partei=c("SVP", "FDP", "CVP", "SP", "EVP", "GPS", "BDP", "glp"),
+                         light=c("#2E9E64", "#6D86B1", "#F8B062", "#EC5962", "#FAE55E", "#A8CE83", "#E2C855", "#D5DB5A"),
+                         normal=c("#008640", "#48689D", "#F69B43", "#E82E3B", "#F8DE4F", "#92C266", "#DABA41", "#CAD249"))
+  
+  if(!is.null(party)) partycol <- subset(partycol, Partei==party)
+
+  if(!is.null(scheme)){
+    assign(se, scheme)
+    partycol <- as.character(partycol[,get(se)])
+  }
+ 
+  return(partycol)
+
 }
 
