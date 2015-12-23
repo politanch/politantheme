ae54eec6d1fdf1d9b018eecd77b5f88d46aeb878
ThomasWilli <willi@uega.ch>
2015-12-23 18:19:30 +0100

    rel
    rel

--- R/politantheme.R
+++ R/politantheme.R
@@ -60,7 +60,7 @@ theme_politan<- function(base_size = 12, base_family = "Helvetica") {
                                         size = 1,
                                         linetype = "solid"),
         plot.title =         element_text(family = base_family,
-                                          size = rel(1.5),
+                                          #size = rel(1.5),
                                           hjust = 0,
                                           vjust = -1,
                                           face="bold"),
